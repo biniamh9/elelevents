ac70d4840e565bcd8d9c3b779d452199f8bc6475
Biniam <biniamh9@gmail.com>
2026-05-09 23:01:24 -0400

    Rentals modification 1

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
-import { NextResponse, type NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   let response = NextResponse.next({
     request,
   });
